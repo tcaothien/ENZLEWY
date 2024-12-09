0eee536aa7118635fc112b728faeffb7de5b135b
tcaothien <116654835+tcaothien@users.noreply.github.com>
2024-12-09 22:20:37 +0700

    Update setWelcomeChannel.js

--- commands/setups/setWelcomeChannel.js
+++ commands/setups/setWelcomeChannel.js
@@ -67,7 +67,7 @@ module.exports = {
         .setAuthor({ 
             name: "Alert!", 
             iconURL: cmdIcons.dotIcon ,
-            url: "https://discord.gg/xQF9f9yUEM"
+            url: "https://discord.gg/enzlewy"
         })
         .setDescription('- This command can only be used through slash command!\n- Please use `/setwelcomechannel`')
         .setTimestamp();
